7b57d989f219bcc1e63508677cfc679e0d361644
Javier Botero <javierbotero10271@gmail.com>
2021-01-26 21:39:45 -0500

    Add linter packages and fix linting errors

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,5 @@
 import React from 'react';
 
-const App = () => {
-  return (
-    <h4>Coming soon, a Bookstore app here!</h4>
-  );  
-}
+const App = () => <h4>Coming soon, a Bookstore app here!</h4>;
 
 export default App;
