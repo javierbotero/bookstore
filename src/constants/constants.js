c1aee33f6a001c561cc6f84710e11f545bd44320
Javier Botero <javierbotero10271@gmail.com>
2021-01-27 21:38:29 -0500

    Add constants inside folder constants

---
+++ src/constants/constants.js
@@ -0,0 +1,12 @@
+const FILTERS = {
+  action: 'Action',
+  biography: 'Biography',
+  history: 'History',
+  horror: 'Horror',
+  kids: 'Kids',
+  Learning: 'Learning',
+  scifi: 'Sci-Fi',
+  all: 'All',
+};
+
+export default FILTERS;
