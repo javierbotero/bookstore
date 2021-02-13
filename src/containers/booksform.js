a9c035473591328245a26ea3ab214d5d265a61f8
Javier Botero <javierbotero10271@gmail.com>
2021-02-13 13:07:30 -0500

    Refactor the component to accept or not the prop book, to make able the same component to display the form when the user wants to update the information of a given book

--- src/containers/booksform.js
+++ src/containers/booksform.js
@@ -4,58 +4,65 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { FILTERS } from '../constants/constants';
 import CategoryFilter from '../components/categoryFilter';
-import { createBook } from '../actions/index';
+import { createBook, updateBook } from '../actions/index';
 import displayErrors from '../helpers/helpers';
 import { removeError } from '../reducers/books';
 
 const BookForm = props => {
-  const [state, setState] = useState({
-    title: '',
-    category: FILTERS.category,
-    author: '',
-    completed: 0,
-  });
   const {
-    sendBook, errors, cleanError, id,
+    sendBook, editBook, errors, cleanError, id, book,
   } = props;
+  const [state, setState] = useState({
+    title: book ? book.title : '',
+    category: book ? book.category : FILTERS.category,
+    author: book ? book.author : '',
+    completed: book ? book.completed : 0,
+  });
   useEffect(() => {
     if (errors) {
-      document.querySelector('.errorForm').classList += ' display-error';
-      displayErrors(errors, 'errorForm');
+      document.querySelector(book ? '.errorUpdate' : '.errorForm').classList += ' display-error';
+      displayErrors(errors, book ? 'errorUpdate' : 'errorForm');
     }
   });
   const handleChange = e => {
     const { name, value } = e.target;
     setState({ ...state, [name]: value });
     cleanError();
-    document.querySelector('.errorForm').classList.remove('display-error');
+    document.querySelector(book ? '.errorUpdate' : '.errorForm').classList.remove('display-error');
   };
   const handleSubmit = async e => {
     e.preventDefault();
+    const form = document.querySelector(book ? '.errorUpdate' : '.errorForm');
     if (state.category !== 'Category' && state.title.length > 0) {
-      await sendBook({ id, book: state });
-      setState({
-        title: '',
-        category: FILTERS.category,
-        author: '',
-      });
-      document.querySelector('input').value = '';
-      document.querySelector('.errorForm').classList.remove('display-error');
+      if (book) {
+        await editBook({ id, book: state });
+      } else {
+        await sendBook({ id, book: state });
+      }
+      if (!book) {
+        setState({
+          title: '',
+          category: FILTERS.category,
+          author: '',
+        });
+        document.querySelector(book ? 'editBook' : 'createBook').value = '';
+      }
+      form.classList.remove('display-error');
     } else {
-      document.querySelector('.errorForm').classList += ' display-error';
-      document.querySelector('.errorForm').innerHTML = 'Please fill the form correctly';
+      form.classList += ' display-error';
+      form.innerHTML = 'Please fill the form correctly';
     }
   };
   return (
     <div>
       <div className="layout form-container">
-        <h3 className="title-form">ADD NEW BOOK</h3>
+        <h3 className="title-form">{book ? 'EDIT THE BOOK' : 'ADD NEW BOOK'}</h3>
         <div className="Error errorForm" />
         <form onSubmit={handleSubmit}>
-          <input type="text" onChange={handleChange} name="title" placeHolder="Book Title" />
+          <input type="text" onChange={handleChange} name="title" className={book ? 'editBook' : 'createBook'} placeHolder="Book Title" />
           <CategoryFilter categories={FILTERS} handleSelectionCreation={handleChange} creation name="category" value={state.category} />
-          <input type="text" onChange={handleChange} name="author" placeHolder="Author" />
-          <button className="Rectangle-2 Rectangle-3" type="submit">ADD BOOK</button>
+          <input type="text" onChange={handleChange} name="author" className={book ? 'editBook' : 'createBook'} placeHolder="Author" />
+          <button className="Rectangle-2 Rectangle-3" type="submit">{book ? 'EDIT' : 'ADD NEW BOOK'}</button>
         </form>
       </div>
     </div>
@@ -64,9 +71,27 @@ const BookForm = props => {
 
 BookForm.propTypes = {
   sendBook: PropTypes.func.isRequired,
-  errors: PropTypes.objectOf(PropTypes.strings).isRequired,
+  editBook: PropTypes.func.isRequired,
+  errors: PropTypes.objectOf(PropTypes.strings),
   cleanError: PropTypes.func.isRequired,
   id: PropTypes.string.isRequired,
+  book: PropTypes.oneOfType([
+    PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      category: PropTypes.string.isRequired,
+      author: PropTypes.string.isRequired,
+      completed: PropTypes.oneOfType([
+        PropTypes.number,
+        PropTypes.string,
+      ]).isRequired,
+    }),
+    PropTypes.bool,
+  ]),
+};
+
+BookForm.defaultProps = {
+  book: false,
+  errors: null,
 };
 
 const mapStateToProps = state => ({
@@ -76,6 +101,7 @@ const mapStateToProps = state => ({
 const mapDispatchToProps = dispatch => ({
   sendBook: data => dispatch(createBook(data)),
   cleanError: () => dispatch(removeError()),
+  editBook: data => dispatch(updateBook(data)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(BookForm);
